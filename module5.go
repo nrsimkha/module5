af998f92aac35a5f100b4c716c04a11c5fc3dcd6
Nrsimkha <sveta.budanaeva@gmail.com>
2024-08-07 15:38:03 -0300

    module 5

---
+++ module5.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i, err := someFunc(0)
+	fmt.Println(i, err)
+
+	i, err = someFunc(10)
+	fmt.Println(i, err)
+}
+
+func someFunc(i int) (int, error) {
+	j, err := funcReturningError(i)
+	if err != nil {
+		return 0, fmt.Errorf("wrap error: %w", err)
+	}
+
+	return j, nil
+}
+
+func funcReturningError(i int) (int, error) {
+	if i == 0 {
+		return 0, fmt.Errorf("got %d", i)
+	}
+
+	return i, nil
+}
